d3e6ebc03d1abfaee0694077bcbf227c24ada193
Nastasia Fourcade <fourcade.nastasia@gmail.com>
2021-02-10 16:09:38 +0100

    finish n°1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+deux_fois_plus_de_piment = { name: 'Deux fois plus de piment', address: '7 rue de beaubourg, Paris 75002', phone_number: '01...', category: 'chinese' }
+ober_mama = { name: 'Ober mama', address: '10 boulevard richard lenoir, Paris 75011', phone_number: '01...', category: 'italian' }
+la_ravigote = { name: 'la ravigote', address: '150 rue menilmontant, Paris 75011', phone_number: '01...', category: 'french' }
+la_frite = { name: 'la frite', address: '20 rue des pyrenné, Paris 75019', phone_number: '01...', category: 'belgian' }
+
+[deux_fois_plus_de_piment, ober_mama, la_ravigote, la_frite].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
